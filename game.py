aa567bd3ec810387a6e18e602e54e9662785d0cb
Pythack <59161870+Pythack@users.noreply.github.com>
2021-11-30 18:16:30 +0100

    Update game.py

--- game.py
+++ game.py
@@ -8,13 +8,56 @@ class move():
     def isLegal(self):
         pass
 
+
 class game():
     def __init__(self, board):
         self.board = board
+        self.turn = "white"
+        self.nTurn = 1
     def move(self, move):
         pass
     def legalMoves(self):
-        pass
+        moves = []
+        case = (0, 0)
+        for row in self.board.layout:
+            for piec in row:
+                if piec:
+                    if piec.type == "rook":
+                        for i in range(1, 8):
+                            if case[1]+i <= 7:
+                                if self.board.layout[case[1]+i][case[0]] == None:
+                                    moves.append(move(self.board, piec, case, (case[0], case[1]+i)))
+                                else:
+                                    break
+                            else:
+                                break
+                        for i in range(1, 8):
+                            if case[1]-i >= 0:
+                                if self.board.layout[case[1]-i][case[0]] == None:
+                                    moves.append(move(self.board, piec, case, (case[0], case[1]-i)))
+                                else:
+                                    break
+                            else:
+                                break
+                        for i in range(1, 8):
+                            if case[0]+i <= 7:
+                                if self.board.layout[case[1]][case[0]+i] == None:
+                                    moves.append(move(self.board, piec, case, (case[0]+i, case[1])))
+                                else:
+                                    break
+                            else:
+                                break
+                        for i in range(1, 8):
+                            if case[0]-i >= 0:
+                                if self.board.layout[case[1]][case[0]-i] == None:
+                                    moves.append(move(self.board, piec, case, (case[0]-i, case[1])))
+                                else:
+                                    break
+                            else:
+                                break
+                case = (case[0]+1, case[1])
+            case = (0, case[1]+1)
+        return moves
         
 class piece():
     def __init__(self, type, color, alive = True):
@@ -29,9 +72,9 @@ class piece():
         pass
     
 class board():
-    def __init__(self, board = [[piece("rook", "white"), piece("knight", "white"), piece("bishop", "white"), piece("king", "white"), piece("queen", "white"), piece("bishop", "white"), piece("knight", "white"), piece("rook", "white")], [piece("pawn", "white"), piece("pawn", "white"), piece("pawn", "white"), piece("pawn", "white"), piece("pawn", "white"), piece("pawn", "white"), piece("pawn", "white"), piece("pawn", "white")], [None, None, None, None, None, None, None, None], [None, None, None, None, None, None, None, None], [None, None, None, None, None, None, None, None], [None, None, None, None, None, None, None, None], [], [piece("rook", "white"), piece("knight", "white"), piece("bishop", "white"), piece("king", "white"), piece("queen", "white"), piece("bishop", "white"), piece("knight", "white"), piece("rook", "white")]]):
+    def __init__(self, board = [[piece("rook", "white"), piece("knight", "white"), piece("bishop", "white"), piece("king", "white"), piece("queen", "white"), piece("bishop", "white"), piece("knight", "white"), piece("rook", "white")], [piece("pawn", "white"), piece("pawn", "white"), piece("pawn", "white"), piece("pawn", "white"), piece("pawn", "white"), piece("pawn", "white"), piece("pawn", "white"), piece("pawn", "white")], [None, None, None, None, None, None, None, None], [None, None, None, None, None, None, None, None], [None, None, None, None, None, None, None, None], [None, None, None, None, None, None, None, None], [piece("pawn", "black"), piece("pawn", "black"), piece("pawn", "black"), piece("pawn", "black"), piece("pawn", "black"), piece("pawn", "black"), piece("pawn", "black"), piece("pawn", "black")], [piece("rook", "black"), piece("knight", "black"), piece("bishop", "black"), piece("king", "black"), piece("queen", "black"), piece("bishop", "black"), piece("knight", "black"), piece("rook", "black")]]):
         self.layout = board
 
 newBoard = board()
 theGame = game(newBoard)
-print(len(theGame.board.layout))
+print(theGame.legalMoves())
