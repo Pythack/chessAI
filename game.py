d1ce97e8b99dd27768807d1336c266bbff7a32a2
Pythack <59161870+Pythack@users.noreply.github.com>
2021-11-30 12:33:40 +0100

    Update game.py

--- game.py
+++ game.py
@@ -1,7 +1,3 @@
-class board():
-    def __init__(self, isDefault = True):
-        pass
-
 class move():
     def __init__(self, board, piece, origin, destination):
         self.piece = piece
@@ -17,12 +13,25 @@ class game():
         self.board = board
     def move(self, move):
         pass
+    def legalMoves(self):
+        pass
         
 class piece():
-    def __init__(self, type, color, game, alive = True):
+    def __init__(self, type, color, alive = True):
+        if not type in ["rook", "knight", "bishop", "king", "queen", "pawn"]:
+            raise TypeError(type, "is not a valid piece type")
+        if not color in ["white", "black"]:
+            raise TypeError(color, "is not a valid chess color")
         self.type = type
         self.color = color
-        self.game = game
         self.alive = alive
     def legalMoves(self):
         pass
+    
+class board():
+    def __init__(self, board = [[piece("rook", "white"), piece("knight", "white"), piece("bishop", "white"), piece("king", "white"), piece("queen", "white"), piece("bishop", "white"), piece("knight", "white"), piece("rook", "white")], [piece("pawn", "white"), piece("pawn", "white"), piece("pawn", "white"), piece("pawn", "white"), piece("pawn", "white"), piece("pawn", "white"), piece("pawn", "white"), piece("pawn", "white")], [None, None, None, None, None, None, None, None], [None, None, None, None, None, None, None, None], [None, None, None, None, None, None, None, None], [None, None, None, None, None, None, None, None], [], []]):
+        self.layout = board
+
+newBoard = board()
+theGame = game(newBoard)
+print(len(theGame.board.layout))
